53074da35cbd79ee45240f643990c6e5e1d60fdf
aerylin <aerylin@Aery-Spectre-x360>
2021-03-17 03:56:22 +0800

    add misty-smooth-launcher

---
+++ misty-smooth-launcher/src/main/java/org/misty/smooth/launcher/MistyDescription$SmoothLauncher.java
@@ -0,0 +1,10 @@
+package org.misty.smooth.launcher;
+
+import org.misty.description.core.MistyDescription;
+
+public class MistyDescription$SmoothLauncher extends MistyDescription {
+
+    public MistyDescription$SmoothLauncher() {
+        super("misty-smooth-launcher", "v1.0.0-SNAPSHOT");
+    }
+}
---
+++ misty-smooth-launcher/src/test/java/org/misty/smooth/launcher/MistyDescription$SmoothLauncherTest.java
@@ -0,0 +1,19 @@
+package org.misty.smooth.launcher;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.misty.description.core.MistyDescription;
+import org.misty.description.core.MistyDescriptionFinder;
+
+import java.util.List;
+
+class MistyDescription$SmoothLauncherTest {
+
+    @Test
+    public void test() {
+        List<MistyDescription> list = MistyDescriptionFinder.findBySPI();
+        list.forEach(System.out::println);
+        Assertions.assertThat(list).contains(new MistyDescription$SmoothLauncher());
+    }
+
+}
