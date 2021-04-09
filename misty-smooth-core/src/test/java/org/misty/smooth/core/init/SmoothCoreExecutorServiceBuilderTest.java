bf9c97ce37dd9c9df242d8b41deffbc383746c87
aerylin <aerylin@Aery-Spectre-x360>
2021-04-09 23:08:22 +0800

    SmoothCoreExecutorServiceBuilderTest done

--- misty-smooth-core/src/test/java/org/misty/smooth/core/init/SmoothCoreExecutorServiceBuilderTest.java
+++ misty-smooth-core/src/test/java/org/misty/smooth/core/init/SmoothCoreExecutorServiceBuilderTest.java
@@ -6,6 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.misty.smooth.core.constant.ThreadPoolArgument;
 import org.misty.smooth.core.context.api.SmoothCoreEnvironment;
 import org.misty.smooth.core.context.impl.SmoothCoreEnvironmentPreset;
+import org.misty.util.error.MistyException;
+import org.misty.util.verify.ExamineIntervals;
+import org.misty.util.verify.ExaminerMessage;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.function.IntConsumer;
+import java.util.function.IntFunction;
 
 class SmoothCoreExecutorServiceBuilderTest {
 
@@ -21,14 +28,109 @@ class SmoothCoreExecutorServiceBuilderTest {
 
     @Test
     public void getCorePoolSize() {
-        Assertions.assertThat(this.builder.getCorePoolSize(this.environment))
-                .isEqualTo(ThreadPoolArgument.CoreSize.PRESET);
+        int size = ThreadPoolArgument.CoreSize.PRESET;
+        Assertions.assertThat(this.builder.getCorePoolSize(this.environment)).isEqualTo(size);
 
-        this.environment.addArgument(ThreadPoolArgument.CoreSize.KEY, "55");
-        Assertions.assertThat(this.builder.getCorePoolSize(this.environment))
-                .isEqualTo(55);
+        size = ThreadPoolArgument.CoreSize.MIX;
+        this.environment.addArgument(ThreadPoolArgument.CoreSize.KEY, size + "");
+        Assertions.assertThat(this.builder.getCorePoolSize(this.environment)).isEqualTo(size);
 
+        size = ThreadPoolArgument.CoreSize.MAX;
+        this.environment.addArgument(ThreadPoolArgument.CoreSize.KEY, size + "");
+        Assertions.assertThat(this.builder.getCorePoolSize(this.environment)).isEqualTo(size);
 
+        IntFunction<String> invalidMsg = (number) -> ExaminerMessage.requireInRange(ThreadPoolArgument.CoreSize.KEY, number,
+                ExamineIntervals.Floor.INCLUDE, ThreadPoolArgument.CoreSize.MIX,
+                ExamineIntervals.Ceiling.INCLUDE, ThreadPoolArgument.CoreSize.MAX);
+
+        size = ThreadPoolArgument.CoreSize.MIX - 1;
+        this.environment.addArgument(ThreadPoolArgument.CoreSize.KEY, size + "");
+        Assertions.assertThatThrownBy(() -> this.builder.getCorePoolSize(this.environment))
+                .isInstanceOf(MistyException.class)
+                .hasMessageContaining(invalidMsg.apply(size));
+
+        size = ThreadPoolArgument.CoreSize.MAX + 1;
+        this.environment.addArgument(ThreadPoolArgument.CoreSize.KEY, size + "");
+        Assertions.assertThatThrownBy(() -> this.builder.getCorePoolSize(this.environment))
+                .isInstanceOf(MistyException.class)
+                .hasMessageContaining(invalidMsg.apply(size));
+    }
+
+    @Test
+    public void getMaximumPoolSize() {
+        int size = ThreadPoolArgument.MaxSize.PRESET;
+        Assertions.assertThat(this.builder.getMaximumPoolSize(this.environment)).isEqualTo(size);
+
+        size = ThreadPoolArgument.MaxSize.MIX;
+        this.environment.addArgument(ThreadPoolArgument.MaxSize.KEY, size + "");
+        Assertions.assertThat(this.builder.getMaximumPoolSize(this.environment)).isEqualTo(size);
+
+        size = ThreadPoolArgument.MaxSize.MAX;
+        this.environment.addArgument(ThreadPoolArgument.MaxSize.KEY, size + "");
+        Assertions.assertThat(this.builder.getMaximumPoolSize(this.environment)).isEqualTo(size);
+
+        IntFunction<String> invalidMsg = (number) -> ExaminerMessage.requireInRange(ThreadPoolArgument.MaxSize.KEY, number,
+                ExamineIntervals.Floor.INCLUDE, ThreadPoolArgument.MaxSize.MIX,
+                ExamineIntervals.Ceiling.INCLUDE, ThreadPoolArgument.MaxSize.MAX);
+
+        size = ThreadPoolArgument.MaxSize.MIX - 1;
+        this.environment.addArgument(ThreadPoolArgument.MaxSize.KEY, size + "");
+        Assertions.assertThatThrownBy(() -> this.builder.getMaximumPoolSize(this.environment))
+                .isInstanceOf(MistyException.class)
+                .hasMessageContaining(invalidMsg.apply(size));
+
+        size = ThreadPoolArgument.MaxSize.MAX + 1;
+        this.environment.addArgument(ThreadPoolArgument.MaxSize.KEY, size + "");
+        Assertions.assertThatThrownBy(() -> this.builder.getMaximumPoolSize(this.environment))
+                .isInstanceOf(MistyException.class)
+                .hasMessageContaining(invalidMsg.apply(size));
+    }
+
+    @Test
+    public void getKeepAliveTime() {
+        int size = ThreadPoolArgument.AliveSecond.PRESET;
+        Assertions.assertThat(this.builder.getKeepAliveTime(this.environment)).isEqualTo(size);
+
+        size = ThreadPoolArgument.AliveSecond.MIX;
+        this.environment.addArgument(ThreadPoolArgument.AliveSecond.KEY, size + "");
+        Assertions.assertThat(this.builder.getKeepAliveTime(this.environment)).isEqualTo(size);
+
+        size = ThreadPoolArgument.AliveSecond.MAX;
+        this.environment.addArgument(ThreadPoolArgument.AliveSecond.KEY, size + "");
+        Assertions.assertThat(this.builder.getKeepAliveTime(this.environment)).isEqualTo(size);
+
+        IntFunction<String> invalidMsg = (number) -> ExaminerMessage.requireInRange(ThreadPoolArgument.AliveSecond.KEY, number,
+                ExamineIntervals.Floor.INCLUDE, ThreadPoolArgument.AliveSecond.MIX,
+                ExamineIntervals.Ceiling.INCLUDE, ThreadPoolArgument.AliveSecond.MAX);
+
+        size = ThreadPoolArgument.AliveSecond.MIX - 1;
+        this.environment.addArgument(ThreadPoolArgument.AliveSecond.KEY, size + "");
+        Assertions.assertThatThrownBy(() -> this.builder.getKeepAliveTime(this.environment))
+                .isInstanceOf(MistyException.class)
+                .hasMessageContaining(invalidMsg.apply(size));
+
+        size = ThreadPoolArgument.AliveSecond.MAX + 1;
+        this.environment.addArgument(ThreadPoolArgument.AliveSecond.KEY, size + "");
+        Assertions.assertThatThrownBy(() -> this.builder.getKeepAliveTime(this.environment))
+                .isInstanceOf(MistyException.class)
+                .hasMessageContaining(invalidMsg.apply(size));
+    }
+
+    @Test
+    public void getThreadFactory() {
+        Runnable r = () -> {
+        };
+
+        String namePrefix = ThreadPoolArgument.NamePrefix.PRESET;
+        ThreadFactory threadFactory = this.builder.getThreadFactory(this.environment);
+        Assertions.assertThat(threadFactory.newThread(r).getName()).isEqualTo(namePrefix + "1");
+        Assertions.assertThat(threadFactory.newThread(r).getName()).isEqualTo(namePrefix + "2");
+
+        namePrefix = "kerker";
+        this.environment.addArgument(ThreadPoolArgument.NamePrefix.KEY, namePrefix);
+        threadFactory = this.builder.getThreadFactory(this.environment);
+        Assertions.assertThat(threadFactory.newThread(r).getName()).isEqualTo(namePrefix + "1");
+        Assertions.assertThat(threadFactory.newThread(r).getName()).isEqualTo(namePrefix + "2");
     }
 
 }
